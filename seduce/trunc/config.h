85cc03838e13f5c80c5088898ab8a0fafc05ab53
skalkoto <skalkoto@3f3173b3-b911-0410-a0ee-e5e1c24a6da8>
2007-04-29 16:16:50 +0000

    Initial import

---
+++ seduce/trunc/config.h
@@ -0,0 +1,17 @@
+/*
+ * Configuration file
+ */
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+/* time to wait for an answer */
+#define TIMEOUT_WAIT 	5
+
+/* times to retry */
+#define RETRY_TIMES	5
+
+/* tme to wait before retying */
+#define RETRY_WAIT	5
+
+#endif /* _CONFIG_H */ 
---
+++ seduce/trunc/detect_engine.c
@@ -0,0 +1,10 @@
+#include "detect_engine.h"
+
+char *threat_payload;
+size_t threat_length;
+
+int execute_work(char *data, size_t len)
+{
+	return WORK_DONE;
+}
+ 
