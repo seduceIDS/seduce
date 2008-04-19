04a3e00e616cb28ac696a4efe06ba1f0d60c418a
Haris Andrianakis <tl0gic@users.noreply.github.com>
2008-04-19 16:19:12 +0000

    Some more fixes for detect_engine_qemu.{c,h}

--- agent/detect_engine_qemu.h
+++ agent/detect_engine_qemu.h
@@ -5,6 +5,7 @@
 #include <sys/time.h> /* for struct itimerval */
 
 #include "qemu.h"     /* for CPUX86State */
+#include "detect_engine.h"
 
 typedef struct _QemuVars {
     struct itimerval value;
@@ -12,5 +13,11 @@ typedef struct _QemuVars {
     unsigned long stack_base;
     CPUX86State *cpu;
 } QemuVars;
+
+static int qemu_engine_process(char *data, size_t len);
+static int qemu_engine_init(void);
+static void qemu_engine_destroy(void);
+static int qemu_engine_get_threat(Threat *t);
+static void qemu_engine_reset(void);
 
 #endif /* _DETECT_ENGINE_QEMU_H */
