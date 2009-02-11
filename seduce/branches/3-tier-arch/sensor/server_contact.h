fabc13f85ba37827e242633a74f36bbf5e7913b5
glynos <glynos@3f3173b3-b911-0410-a0ee-e5e1c24a6da8>
2009-02-11 10:34:57 +0000

    3-tier-arch/sensor/ 	* made stream_data_break -> break_stream_data to fit the naming 	convention of other functions 	* added the type parameter to new_stream_connection and send_dgram_data 	(as included in the paper) 	* type is not used internally yet.

--- seduce/branches/3-tier-arch/sensor/server_contact.h
+++ seduce/branches/3-tier-arch/sensor/server_contact.h
@@ -14,18 +14,19 @@ struct tuple4 /* for new_stream_connection and send_dgram_data */
 };
 #endif
 
+enum data_t { UNKNOWN, DGRAM_DATA, STREAM_DATA, HTTP, SMTP, DNS };
 
 int manager_connect(int *sockfd, in_addr_t addr, unsigned short port);
 int manager_disconnect(int sockfd);
 
-int new_stream_connection(int sockfd, const struct tuple4 *conn, 
-			  unsigned **id);
+int new_stream_connection(int sockfd, const struct tuple4 *conn, unsigned **id,
+			  enum data_t type);
 int close_stream_connection(int sockfd, unsigned *id);
 
 int send_stream_data(int sockfd, unsigned id, const void *data, size_t len);
-int stream_data_break(int sockfd, unsigned id);
+int break_stream_data(int sockfd, unsigned id);
 
 int send_dgram_data(int sockfd, const struct tuple4 *conn, const void *data,
-		    size_t len);
+		    size_t len, enum data_t type);
 
 #endif /* _SERVER_CONTACT_H */
