e2947a57e46a6755e14eb5fea867ea86fd4dad66
glynos <glynos@3f3173b3-b911-0410-a0ee-e5e1c24a6da8>
2009-06-26 17:41:58 +0000

    delete

--- seduce/branches/python-opty2/trunk/agent/alert.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ALERT_H
-#define _ALERT_H
-
-#include "sensor_contact.h"
-#include "detect_engine.h"
-
-int submit_alert(const SensorSession *, const ConnectionInfo *, const Threat *);
-
-#endif /* _ALERT_H */
