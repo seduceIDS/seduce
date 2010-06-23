eaa566a6a6d4a9b466a411659fd6ad600f96f4ae
glynos <glynos@3f3173b3-b911-0410-a0ee-e5e1c24a6da8>
2010-06-23 16:13:15 +0000

    tagging qemu-0.12.4 with raw binary loading support

--- agent/alert.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ALERT_H
-#define _ALERT_H
-
-#include "manager_protocol.h"
-#include "detection_engine.h"
-
-int submit_alert(const ManagerSession *, const ConnectionInfo *, const Threat *);
-
-#endif /* _ALERT_H */
--- sensor/sniffer.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SNIFFER_H
-#define _SNIFFER_H
-
-int init_sniffer(void);
-void start_sniffer(void);
-
-#endif /* _SNIFFER_H */
