4a418141ea2f99ff2eaa7dc17814d53675c0760a
Dimitris Glynos <dimitris@census-labs.com>
2021-10-02 23:06:29 +0300

    apply patch to agent/libemu

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 git submodule update --init
-# libemu v1.0.3 build breaks due to a warning
-sed -i 's/ -Werror//' agent/libemu/src/Makefile.am
+# libemu v1.0.4 build breaks due to a warning
+cd agent
+sed -i 's/ -Werror//' libemu/src/Makefile.am
+patch -p2 < libemu_shellcode.patch
+cd ..
 aclocal
 autoheader
 automake --add-missing
