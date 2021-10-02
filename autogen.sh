05a76a081cf2fb100f6b02743f516a4268a9f614
Dimitris Glynos <dimitris@census-labs.com>
2021-10-02 15:37:11 +0300

    temp fix for libemu v1.0.3 build bug in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 git submodule update --init
+# libemu v1.0.3 build breaks due to a warning
+sed -i 's/ -Werror//' agent/libemu/src/Makefile.am
 aclocal
 autoheader
 automake --add-missing
