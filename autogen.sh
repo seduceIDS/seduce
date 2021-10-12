233622faaca0021112ac17f268f796aa7af439b1
Dimitris Glynos <dimitris@census-labs.com>
2021-10-13 00:04:23 +0300

    simplify autogen.sh now that our changes to libemu exist in a separate repo

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 git submodule update --init
-# libemu v1.0.4 build breaks due to a warning
-cd agent
-sed -i 's/ -Werror//' libemu/src/Makefile.am
-patch -p2 < libemu_shellcode.patch
-cd ..
 aclocal
 autoheader
 automake --add-missing
