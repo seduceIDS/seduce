a627786a12d4feca4c97967cf2e5a610d806c541
kat <kat.derv98@gmail.com>
2024-07-22 17:19:57 +0300

    deleted old dir

--- agent/behavour/core/config.py
+++
@@ -1,5 +0,0 @@
-FASTTEXT1 = "agent/behaviour/models/model.bin"
-FASTTEXT2 = "agent/behaviour/models/model2.bin"
-
-SVM1 = "agent/behaviour/models/svm_model.onnx"
-SVM2 = "agent/behaviour/models/svm_model2.onnx"
